47798d0d7a76cd682e813d493b7e6aa12d93dd55
dartbill <billieldartnell@hotmail.com>
2022-06-13 14:02:53 +0100

    add different album art

--- src/components/PokemonCards.js
+++ src/components/PokemonCards.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const Artists = () => {
   const [artists, setArtists] = useState([
     { album: 'Sleep is for the week', songs: "The Real Damage, Vital Signs, Romantic Fatigue, A Decent Cup Of Tea, Fathers Day, Worse Things Happen At Sea, My Kingdom For A Horse, Back In The Day Once We Were Anarchists, Wisdom Teeth, The Ladies Of London Town, Must Try Harder, The Ballad Of Me And My Friends",releaseDate:"15.01.07", coverArt:"https://cdn1.umg3.net/269/files/2013/03/Frank_Turner_Sleep_is_for_the_Week-318x313.jpg" },
+    { album: 'Love Ire and Song', songs: "I Knew Prufrock Before He Got Famous, Reasons Not To Be An Idiot, Photosynthesis, Substitute, Better Half, Love Ire and Song, Imperfect Tense, To Take You Home, Long Live The Queen, A Love Worth Keeping, St Christopher Is Coming Home, Jet Lag",releaseDate:"31.03.08", coverArt:"https://cdn1.umg3.net/269/files/2013/03/FrankturnerLoveIreSong.jpg" }
   ])
 
   const renderRows = () => {
